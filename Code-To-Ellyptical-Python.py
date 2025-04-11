2e0d36f14f9b4bb4bbb0fc242255d9c84d1416e7
fede0009000 <76182933+Fedank@users.noreply.github.com>
2025-04-11 21:26:47 +0200

    Added comments

--- Code-To-Ellyptical-Python.py
+++ Code-To-Ellyptical-Python.py
@@ -1,15 +1,16 @@
 
 '''
-Struttura
-exec()
-'%c' * numero di caratteri totali % tuple contenente il numero ASCII di ogni lettera
+Structure:
+exec(%c * n_characters % (tuple containing ASCII numbers for each character in order))
 '''
 
+# Read from file
 file_path = input("File path (WARNING: the file will be modified irreversibly into ellyptical python): ")
 with open(file_path, 'r') as file:
     lines = file.readlines()
     string = ''.join(lines)
 
+# Convert the code to ellyptical python
 result = "("
 
 for c in string:
@@ -18,7 +19,9 @@ for c in string:
 result = result.strip(",")
 result += ")"
 
+# Add the exec() part to actually make it execute
 write_to_file = "exec('%c' * " + str(len(string)) + " % " + result + ")"
 
+# Overwrite original file
 with open(file_path, 'w') as file:
     file.write(write_to_file)
